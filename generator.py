891d4449a241cae64a6aef04a3fb012017e8779c
idontcar3 <dunnothrasher@gmail.com>
2020-02-09 04:27:40 -0500

    Generator

--- generator.py
+++ generator.py
@@ -15,6 +15,12 @@ products_map = dict()
 # for customer registration date lookup
 customers_map = dict()
 
+# for products prices
+products_prices_map = dict()
+
+# for order ids
+orders_map = dict()
+
 
 class Products(testdata.DictFactory):
     product_id = testdata.RandomInteger(60, 100)
@@ -42,19 +48,23 @@ class Customer(testdata.DictFactory):
 
 class Orders(testdata.DictFactory):
     order_id = testdata.RandomInteger(500, 530)
-    # customer_id = testdata.RandomInteger(300, 330)
+    customer_id = testdata.RandomInteger(300, 330)
 
 
 class OrderDetails(testdata.DictFactory):
     order_detail_id = testdata.RandomInteger(600, 630)
-    order_id = testdata.RandomInteger(500, 530)
-    product_id = testdata.RandomInteger(60, 100)
+    # order_id = testdata.RandomInteger(500, 530)
+    # product_id = testdata.RandomInteger(60, 100)
 
 
 class ProductReviews(testdata.DictFactory):
     product_review_id = testdata.RandomInteger(700, 730)
     product_id = testdata.RandomInteger(60, 100)
     review_score = testdata.RandomInteger(1, 10)
+
+
+def format_date(date):
+    return date.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
 def random_date(start, end):
@@ -64,55 +74,63 @@ def random_date(start, end):
     return start + datetime.timedelta(seconds=random_second)
 
 
-def save_csv(filename, header, objects):
+def save_csv(filename, objects):
     # save products
     with open(filename, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=header)
-        writer.writeheader()
-        for o in objects:
-            writer.writerow(o)
+        # writer = csv.DictWriter(csvfile, fieldnames=header)
+        # writer.writeheader()
+        writer = csv.writer(csvfile)
+        for i, o in enumerate(objects):
+
+            if i == 0:
+                writer.writerow(o.keys())
+
+            row = list()
+            # format date objects
+            for k in o.keys():
+                if isinstance(o[k], datetime.date):
+                    o[k] = format_date(o[k])
+                row.append(o[k])
+
+            writer.writerow(row)
 
 
 def generate_products(generate_num):
 
-    products_list = list()
     for p in Products().generate(generate_num):
         p["category_id"] = products[p["product_name"]]["category"]
 
         products_map[p["product_id"]] = p
-        products_list.append(p)
-        return products_list
+        yield p
 
 
 def generate_product_price(generate_num):
     # Generar ProductPrices
-    products_price_list = list()
     for pp in ProductPrice().generate(generate_num):
         # get the associated product
-        # p = products_map[pp["product_id"]]
         product_id = choice(products_map.keys())
         p = products_map[product_id]
-        pp["seller_id"] = p["seller_id"]
-        products_price_list.append(pp)
 
-    return products_price_list
+        try:
+            products_prices_map[p["product_id"]].append(pp["product_price_id"])
+        except KeyError:
+            products_prices_map[p["product_id"]] = [pp["product_price_id"]]
+
+        pp["seller_id"] = p["seller_id"]
+        yield pp
 
 
 def generate_customers(generate_num):
-    customers_list = list()
     for c in Customer().generate(generate_num):
         c["register_on"] = random_date(
             datetime.datetime(2018, 10, 1, 1, 1, 0, 0),
             datetime.datetime.now()
         )
         customers_map[c["customer_id"]] = {"register_on": c["register_on"]}
-        customers_list.append(c)
-
-    return customers_list
+        yield c
 
 
 def generate_orders(generate_num):
-    orders_list = list()
     for o in Orders().generate(generate_num):
         customer = choice(customers_map.keys())
         o["register_at"] = random_date(
@@ -120,49 +138,45 @@ def generate_orders(generate_num):
             datetime.datetime.now()
         )
 
-        orders_list.append(o)
+        orders_map[o["order_id"]] = o
 
-    return orders_list
+        yield o
 
 
 def generate_order_details(generate_num):
-    order_details_list = list()
     for od in OrderDetails().generate(15):
-        order_details_list.append(od)
-
-    return order_details_list
+        od["order_id"] = choice(orders_map.keys())
+        od["product_price_id"] = choice(products_prices_map.keys())
+        yield od
 
 
 def generate_products_reviews(generate_num):
-    products_reviews_list = list()
     for pr in ProductReviews().generate(15):
-        products_reviews_list.append(pr)
-
-    return products_reviews_list
+        yield pr
 
 
 if __name__ == "__main__":
 
     # generate products
     products_list = generate_products(15)
-    save_csv("products.csv", products_list[0].keys(), products_list)
+    save_csv("products.csv", products_list)
 
     # generate products_price
     products_price_list = generate_product_price(15)
-    save_csv("product_prices.csv", products_price_list[0].keys(), products_price_list)
+    save_csv("product_prices.csv", products_price_list)
 
     # generate customers
     customers_list = generate_customers(15)
-    save_csv("customers.csv", customers_list[0].keys(), customers_list)
+    save_csv("customers.csv", customers_list)
 
     # genereate orders
     orders_list = generate_orders(15)
-    save_csv("orders.csv", orders_list[0].keys(), orders_list)
+    save_csv("orders.csv", orders_list)
 
     # generate order details
     order_details_list = generate_order_details(15)
-    save_csv("order_details.csv", order_details_list[0].keys(), order_details_list)
+    save_csv("order_details.csv", order_details_list)
 
     # generate product reviews
     product_reviews_list = generate_products_reviews(15)
-    save_csv("product_reviews.csv", product_reviews_list[0].keys(), product_reviews_list)
+    save_csv("product_reviews.csv", product_reviews_list)
